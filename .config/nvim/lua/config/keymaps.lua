3ae812619f0f5686271987587d88744babda7813
xuehui <xuehuizzz103@gmail.com>
2025-03-20 16:24:50 +0800

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.set('v', '<S-Tab>', '<gv')
 vim.keymap.set('n', '<Tab>', '>>_')
 vim.keymap.set('n', '<S-Tab>', '<<_')
 
+-- 设置 Ctrl+A 全选
+vim.keymap.set('n', '<C-a>', 'ggVG')  -- 普通模式下的全选
+vim.keymap.set('i', '<C-a>', '<Esc>ggVG')  -- 插入模式下的全选
+vim.keymap.set('v', '<C-a>', '<Esc>ggVG')  -- 可视模式下的全选
