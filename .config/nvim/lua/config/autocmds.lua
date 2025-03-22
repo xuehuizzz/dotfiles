8ebfbaaec47e65dd33991fc7b366a2700df6502f
xuehui <xuehuizzz103@gmail.com>
2025-03-22 16:33:43 +0800

    Update autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,22 +1,22 @@
 -- 记住上次编辑位置
-vim.api.nvim_create_autocmd('BufReadPost', {
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
 
 -- 退出插入模式时, 自动关闭粘贴模式
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste"
+	pattern = "*",
+	command = "set nopaste",
 })
 
 -- 打开下列类型文件时, 关闭代码隐藏功能
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown" },
-  command = "set conceallevel=0"
+	pattern = { "json", "jsonc", "markdown" },
+	command = "set conceallevel=0",
 })
