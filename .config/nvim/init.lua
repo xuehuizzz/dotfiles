444de75e0d89435f57a92ded8d80fb3e42687fd0
xuehui <xuehuizzz103@gmail.com>
2025-03-22 16:50:40 +0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,4 @@
--- 安装 lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- 设置 leader 键，建议在初始化 lazy 之前设置
-vim.g.mapleader = " "  -- 设置全局 leader 键为空格键
-vim.g.maplocalleader = " "   -- 设置局部 leader 键为空格键
-
 require("config.keymaps")   -- 快捷键
 require("config.options")   -- 基础配置
 require("config.autocmds")  -- 自定义命令
-require("lazy").setup({     -- 加载插件
-	require "plugins.colortheme",
-	require 'plugins.autocompletion',
-	require 'plugins.neotree',
-	require 'plugins.indent-blankline',
-	require 'plugins.treesitter',
-	require 'plugins.lualine',
-	require 'plugins.comment',
-	require 'plugins.toggleterm',
-}
+require("config.lazy")      -- 加载插件 lazy.nvim
