ab59d198f2695591f08ccd19cfeb4f450b0cda5b
xuehui <xuehuizzz103@gmail.com>
2025-03-25 15:37:46 +0800

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,10 @@
 return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "p00f/nvim-ts-rainbow",
+  },
   build = ":TSUpdate",
   main = "nvim-treesitter.configs", -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
       "lua",
@@ -40,6 +42,16 @@ return { -- Highlight, edit, and navigate code
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { "ruby" },
+    },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = nil,
+      colors = {
+        "#F9D749",
+        "#CC78D1",
+        "#479FF8",
+      },
     },
     indent = { enable = true, disable = { "ruby" } },
   },
