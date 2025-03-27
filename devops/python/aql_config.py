55c8be930648c807b70d6f24cf7b08fbcadde1e1
xuehui <xuehuizzz103@gmail.com>
2025-03-27 17:24:11 +0800

    Update aql_config.py

--- devops/python/aql_config.py
+++ devops/python/aql_config.py
@@ -14,6 +14,26 @@ class AQLConfig:
             logger.error("数据库连接失败: %s", str(e))
             raise
 
+    def insert_data(self, data):
+        # data格式为:  list[dic1, dic2, dic3, ...]
+        # 已存在的文档不会插入
+        if not data:
+            logger.warning("数据异常, 无需插入")
+        logger.info("插入数据开始")
+        try:
+            # 已存在的文档不会插入
+            collection = self.db.collections['xxx']
+            batch_size = 10000
+            for i in range(0, len(data), batch_size):
+                batch = data[i:i + batch_size]
+                docs = [collection.createDocument(item) for item in batch]
+                collection.bulkSave(docs)
+                logger.info("已插入: %r 条数据", i + len(batch))
+
+            logger.info("插入数据成功")
+        except Exception as e:
+            logger.error("插入数据失败: %r", e)
+
     def run(self):
         try:
             update_aql = f"""xxx"""
