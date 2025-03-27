4aec8250f65be95fba2df823cacc2d24f42c206e
xuehui <xuehuizzz103@gmail.com>
2025-03-28 00:03:40 +0800

    Update nvimtree.lua

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -7,16 +7,17 @@ return {
   config = function()
     local nvimtree = require("nvim-tree")
   
-    -- recommended settings from nvim-tree documentation
+    -- 禁用默认文件浏览器netrw
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
   
     nvimtree.setup({
       view = {
         width = 35,
-        relativenumber = true,
+        -- relativenumber = true,  -- 显示相对行号
       },
-      -- change folder arrow icons
+  
+      -- 设置文件/夹 图标
       renderer = {
         indent_markers = {
           enable = true,
@@ -24,9 +25,16 @@ return {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+              arrow_closed = "", -- 文件夹关闭的箭头
+              arrow_open = "", -- 文件夹打开的箭头
             },
+          },
+  
+          -- show中配置为隐藏所有图标信息, 整个注释掉即可恢复展示
+          show = {
+            file = false, -- 不显示文件图标
+            folder = false, -- 不显示文件夹图标
+            folder_arrow = false, -- 不显示文件夹箭头
           },
         },
       },
@@ -41,24 +49,31 @@ return {
         },
       },
       filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
+        git_ignored = false,
+        dotfiles = false,     -- 不隐藏点文件
+        custom = {          -- 隐藏如下文件
+          "^.git$",
+          "^node_modules$",
+          "^dist$",
+          "^.next$",
+          "^.idea$",
+          "^.vscode$",
+          "\\.pyc$",
+          "__pycache__",
+          ".DS_Store",
+        },
       },
     })
   
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-  
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- 打开/折叠目录
     keymap.set(
       "n",
       "<leader>ef",
       "<cmd>NvimTreeFindFileToggle<CR>",
       { desc = "Toggle file explorer on current file" }
-    ) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    ) -- 定为到当前文件在文件树中的位置
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- 折叠文件树
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- 刷新文件树
   end,
 }
