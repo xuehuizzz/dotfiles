a90e021fe60069ef9fdcb14f136ea5bc7b53bba6
xuehui <xuehuizzz103@gmail.com>
2025-03-28 09:33:55 +0800

    Update mason.lua

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -5,15 +5,10 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    -- import mason
     local mason = require("mason")
-  
-    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
-  
     local mason_tool_installer = require("mason-tool-installer")
   
-    -- enable mason and configure icons
     mason.setup({
       ui = {
         icons = {
@@ -25,7 +20,6 @@ return {
     })
   
     mason_lspconfig.setup({
-      -- list of servers for mason to install
       ensure_installed = {
         "tsserver",
         "html",
@@ -39,6 +33,7 @@ return {
         "pyright",
         "gopls",  
       },
+      automatic_installation = true  
     })
   
     mason_tool_installer.setup({
