a281d715dfe23b927f36a7ddb7328b8d44b11213
xuehui <xuehuizzz103@gmail.com>
2025-03-30 00:18:53 +0800

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,14 @@
 -- 可视模式下的 Tab 缩进
-vim.keymap.set('v', '<Tab>', '>gv')
-vim.keymap.set('v', '<S-Tab>', '<gv')
+vim.keymap.set('v', '<Tab>', '>gv', { noremap = true, silent = true })
+vim.keymap.set('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
 
 -- 普通模式下的 Tab 缩进
-vim.keymap.set('n', '<Tab>', '>>_')
-vim.keymap.set('n', '<S-Tab>', '<<_')
+vim.keymap.set('n', '<Tab>', '>>_', { noremap = true, silent = true })
+vim.keymap.set('n', '<S-Tab>', '<<_', { noremap = true, silent = true })
 
 -- 设置 Ctrl+A 全选
-vim.keymap.set('n', '<C-a>', 'ggVG')
-vim.keymap.set({'i', 'v'}, '<C-a>', '<Esc>ggVG')
+vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
+vim.keymap.set({'i', 'v'}, '<C-a>', '<Esc>ggVG', { noremap = true, silent = true })
+
+-- 禁用 Command+A
+vim.keymap.set({'n', 'i', 'v'}, '<D-a>', '<Nop>', { noremap = true, silent = true })
