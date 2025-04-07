8b36635dff22de09b8dd97170b53ed38c3bfb07d
xuehui <xuehuizzz103@gmail.com>
2025-04-07 17:11:59 +0800

    Update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -34,9 +34,10 @@ vim.opt.visualbell = false -- 关闭可视响铃
 vim.opt.magic = true -- 设置魔术
 vim.opt.backspace = "indent,eol,start" -- 设置退格键行为
 vim.opt.foldenable = true -- 开启折叠
-vim.opt.foldmethod = "syntax" -- 语法折叠
+vim.opt.foldmethod = "manual" -- 手动折叠
 vim.opt.foldcolumn = "0" -- 折叠区域宽度
 vim.opt.foldlevel = 1 -- 折叠层数
+vim.opt.foldlevelstart = 99 -- 默认展开所有折叠
 vim.opt.termguicolors = true -- 启用终端真彩色支持
 vim.opt.background = "dark" -- 设置深色背景主题
 vim.opt.signcolumn = "yes" -- 始终显示标记列, (用于显示 git 状态、错误提示等), 防止文本左右移动
@@ -53,52 +54,51 @@ vim.opt.breakindent = true -- 保持换行后的缩进
 vim.opt.showbreak = "↪ " -- 显示换行符号
 vim.opt.filetype = "on" -- 启用文件类型检测
 vim.opt.syntax = "enable" -- 启用语法高亮
-vim.opt.encoding = "utf-8"  -- 设置编码为 UTF-8
-vim.opt.fileencoding = "utf-8"  -- 设置文件编码为 UTF-8
-vim.opt.langremap = false  -- 禁用语言映射
-vim.opt.virtualedit = "onemore"  -- 允许光标移动到最后一个字符之后
-vim.opt.scroll = 10  -- 设置 Ctrl-U 和 Ctrl-D 滚动的行数
-vim.opt.scrolljump = 1  -- 当光标移出屏幕时滚动的行数
-vim.opt.scrolloff = 8  -- 保持光标上下文可见行数，让滚动更平滑
-
+vim.opt.encoding = "utf-8" -- 设置编码为 UTF-8
+vim.opt.fileencoding = "utf-8" -- 设置文件编码为 UTF-8
+vim.opt.langremap = false -- 禁用语言映射
+vim.opt.virtualedit = "onemore" -- 允许光标移动到最后一个字符之后
+vim.opt.scroll = 10 -- 设置 Ctrl-U 和 Ctrl-D 滚动的行数
+vim.opt.scrolljump = 1 -- 当光标移出屏幕时滚动的行数
+vim.opt.scrolloff = 8 -- 保持光标上下文可见行数，让滚动更平滑
 vim.cmd("language en_US.UTF-8")
 
 -- 完全禁用诊断图标和符号
 -- TODO: 过滤 #501, #402 告警
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = "", -- 设置虚拟文本的前缀为空
-    spacing = 4, -- 设置间距
-    source = true,
-    severity = {
-      min = vim.diagnostic.severity.INFO
-    },
-  },
-  signs = false,  -- 禁用行号列的图标
-  update_in_insert = false,
-  underline = true,
-  severity_sort = true,
-  float = {
-    border = "rounded",
-    source = true,
-    header = "",
-    prefix = "",
-  },
+	virtual_text = {
+		prefix = "", -- 设置虚拟文本的前缀为空
+		spacing = 4, -- 设置间距
+		source = true,
+		severity = {
+			min = vim.diagnostic.severity.INFO,
+		},
+	},
+	signs = false, -- 禁用行号列的图标
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+		header = "",
+		prefix = "",
+	},
 })
 
 -- 禁用一些不需要的内置插件
 local disabled_built_ins = {
-  "netrw",
-  "netrwPlugin",
-  "netrwSettings",
-  "netrwFileHandlers",
-  "gzip",
-  "zip",
-  "zipPlugin",
-  "tar",
-  "tarPlugin",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-  vim.g["loaded_" .. plugin] = 1
+	vim.g["loaded_" .. plugin] = 1
 end
