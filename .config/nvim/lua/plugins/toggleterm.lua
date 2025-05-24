a2f0b4b93ddfe8da71fde429a569bf89f47a44b9
xuehui <xuehuizzz103@gmail.com>
2025-05-24 16:53:04 +0800

    Update toggleterm.lua

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,33 +1,33 @@
 return {
-	"akinsho/toggleterm.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("toggleterm").setup({
-			size = function(term)
-				if term.direction == "horizontal" then
-					return 15
-				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.4
-				end
-			end,
-			open_mapping = [[<c-j>]],
-			shade_filetypes = {},
-			shade_terminals = true,
-			shading_factor = 2,
-			start_in_insert = true,
-			insert_mappings = true,
-			terminal_mappings = true,
-			persist_size = true,
-			persist_mode = true,
-			direction = "horizontal",
-			close_on_exit = true,
-			clear_env = false,
-			auto_scroll = true,
-			shell = vim.o.shell,
-			on_create = function()
-				vim.opt.laststatus = 3 -- 使用全局状态栏
-			end,
-		})
-	end,
+  "akinsho/toggleterm.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<c-j>]],
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      persist_mode = true,
+      direction = "horizontal",
+      close_on_exit = true,
+      clear_env = false,
+      auto_scroll = true,
+      shell = vim.o.shell,
+      on_create = function()
+        vim.opt.laststatus = 3 -- 使用全局状态栏
+      end,
+    })
+  end,
 }
