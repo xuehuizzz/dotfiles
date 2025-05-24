9b84e743db55254c1b6745f8e897c2b2658b7127
xuehui <xuehuizzz103@gmail.com>
2025-05-24 16:53:32 +0800

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,58 @@
 return { -- Highlight, edit, and navigate code
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"p00f/nvim-ts-rainbow",
-	},
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	opts = {
-		ensure_installed = {
-			"lua",
-			"python",
-			"javascript",
-			"typescript",
-			"vimdoc",
-			"vim",
-			"regex",
-			"terraform",
-			"sql",
-			"dockerfile",
-			"toml",
-			"json",
-			"java",
-			"groovy",
-			"go",
-			"gitignore",
-			"graphql",
-			"yaml",
-			"make",
-			"cmake",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"tsx",
-			"css",
-			"html",
-		},
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		rainbow = {
-			enable = true,
-			extended_mode = true,
-			max_file_lines = nil,
-			colors = {
-				"#F9D749",
-				"#CC78D1",
-				"#479FF8",
-			},
-		},
-		indent = { enable = true, disable = { "ruby" } },
-	},
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "p00f/nvim-ts-rainbow",
+  },
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  opts = {
+    ensure_installed = {
+      "lua",
+      "python",
+      "javascript",
+      "typescript",
+      "vimdoc",
+      "vim",
+      "regex",
+      "terraform",
+      "sql",
+      "dockerfile",
+      "toml",
+      "json",
+      "java",
+      "groovy",
+      "go",
+      "gitignore",
+      "graphql",
+      "yaml",
+      "make",
+      "cmake",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "tsx",
+      "css",
+      "html",
+    },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { "ruby" },
+    },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = nil,
+      colors = {
+        "#F9D749",
+        "#CC78D1",
+        "#479FF8",
+      },
+    },
+    indent = { enable = true, disable = { "ruby" } },
+  },
 }
