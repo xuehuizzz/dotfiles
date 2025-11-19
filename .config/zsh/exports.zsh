f1c456abe04a5384c772b964b410e4c7a3c2f294
xuehui <xuehuizzz103@gmail.com>
2025-11-19 12:37:14 +0800

    Update exports.zsh

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -2,12 +2,15 @@ export XDG_CONFIG_HOME="$HOME/.config"  # 配置文件
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
+# homebrew conf
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 export HOMEBREW_AUTO_UPDATE_SECS=1296000  # 15天自动更新一次
 # export HOMEBREW_NO_ENV_HINTS=1  # 隐藏提示信息
 
+# editor conf
 export EDITOR=nvim
 
+# language conf
 export TERM="xterm-256color"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
