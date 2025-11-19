7f658b2bcdbc872cbe34b41a3d57159acd44da65
xuehui <xuehuizzz103@gmail.com>
2025-11-19 12:52:21 +0800

    Update aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,3 +1,5 @@
+# rsync -avz file.txt user@yourserver.com:/home/user/  # 从本地传输到远程服务器
+# rsync -avz user@yourserver.com:/home/user/file.txt file.txt  # 从远程服务器下载文件到本地
 alias ls="ls -G"
 alias ll="ls -lhSr"  # h 人性化显示, S 文件大小排序, r 倒序
 alias rsync="rsync -avz --progress" # 适合大文件传输, 支持断点续传
@@ -7,13 +9,14 @@ alias mkdir="mkdir -pv"  # mkdir folder recursively and explain
 alias c="clear"
 alias sudo="sudo "
 alias reload="exec ${SHELL} -l"
-alias purge="sudo purge"
 alias hist="history -i"
 alias jupyter="nohup jupyter notebook --allow-root > jupyter.log 2>&1 &"  #后台启动jupyter notebook
 alias tarc="tar -czvf"  # 压缩文件   tarc xxx.tar.gz file1 file2 folder1  # 可以同时归档文件和目录并压缩
 alias tarx="tar -xzvf"  # 解压文件   tarx xxx.tar.gz
-alias dsm="docker-slim build --http-probe=false --continue-after=exec"  # dsm imageName:tag   # 镜像瘦身
-alias ql="qlmanage -p"  # quick look
+alias curl="curl --compressed"
+alias wget="wget -c"
+alias nc="nc -zv"  # 现代化测试主机端口连通性工具, nc -zv myserver.com 22
+# alias jupyter="nohup jupyter notebook --allow-root > jupyter.log 2>&1 &"  #后台启动jupyter notebook
 alias_list=(
     "vim:nvim"
     "vi:nvim"
@@ -33,13 +36,28 @@ for pair in "${alias_list[@]}"; do
     fi
 done
 
-# custom functions
-custom_open() {    # for macOS
+
+ffmpeg() {
+  docker run --rm -v "$(pwd):/home/ffmpeg" ffmpeg:v1 -y "$@"
+}
+
+# linux...............
+alias jal="journalctl -xeu"
+alias ps="ps aux --sort=-%cpu"  # 按CPU使用率排序
+# .tar.gz(带-z参数)    .tar.bz2(带-j参数)
+alias tarc="tar -czvf"  # 压缩文件   tarc xxx.tar.gz file1 file2 folder1  # 可以同时归档文件和目录并压缩
+alias tarx="tar -xzvf"  # 解压文件   tarx xxx.tar.gz
+# alias dsm="docker-slim build --http-probe=false --continue-after=exec"  # --host 指定docker socket文件
+
+# macOS......................................................
+# alias ty="sudo spctl --master-disable"  #同意安装任意来源 for mac, 版本需低于macOS15.0
+alias dsm="docker-slim build --http-probe=false --continue-after=exec"  # dsm imageName:tag   # 镜像瘦身
+alias ql="qlmanage -p"  # quick look
+
+custom_open() {
     if [[ -f "$1" ]]; then
-        # 如果是文件，直接用 TextEdit 打开
         command open -a TextEdit "$1"
     elif [[ -d "$1" ]]; then
-        # 如果是目录，直接用 Finder 打开
         command open "$1"
     else
         command open "$@"
@@ -47,7 +65,6 @@ custom_open() {    # for macOS
 }
 alias open="custom_open"
 
-ffmpeg() {
-  docker run --rm -v "$(pwd):/home/ffmpeg" ffmpeg:v1 -y "$@"
-}
+
+
 
