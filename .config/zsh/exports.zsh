5a38049a5db39c699eb25ab61fff700471ad54a1
xuehui <xuehuizzz103@gmail.com>
2025-11-20 22:16:45 +0800

    Update exports.zsh

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -3,7 +3,7 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 # homebrew conf
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 export HOMEBREW_AUTO_UPDATE_SECS=1296000  # 15天自动更新一次
 # export HOMEBREW_NO_ENV_HINTS=1  # 隐藏提示信息
 
