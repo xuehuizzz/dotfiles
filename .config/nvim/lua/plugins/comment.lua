7486e8ac34c8f088e24bcc6966cb8e0f52422efd
xuehui <xuehuizzz103@gmail.com>
2026-03-26 17:39:10 +0800

    Update comment.lua

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,29 +1,20 @@
--- Easily comment visual regions/lines
 return {
-	"numToStr/Comment.nvim",
-	opts = {},
-	config = function()
-		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<C-_>", require("Comment.api").toggle.linewise.current, opts)
-		vim.keymap.set("n", "<C-c>", require("Comment.api").toggle.linewise.current, opts)
-		vim.keymap.set("n", "<C-/>", require("Comment.api").toggle.linewise.current, opts)
-		vim.keymap.set(
-			"v",
-			"<C-_>",
-			"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			opts
-		)
-		vim.keymap.set(
-			"v",
-			"<C-c>",
-			"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			opts
-		)
-		vim.keymap.set(
-			"v",
-			"<C-/>",
-			"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			opts
-		)
-	end,
+  "numToStr/Comment.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("Comment").setup()
+
+    local api = require("Comment.api")
+    local opts = { noremap = true, silent = true }
+
+    -- Normal 模式：切换当前行注释
+    vim.keymap.set("n", "<C-/>", api.toggle.linewise.current, opts)
+
+    -- Visual 模式：切换选中行注释
+    vim.keymap.set("v", "<C-/>", function()
+      local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+      vim.api.nvim_feedkeys(esc, "nx", false)
+      api.toggle.linewise(vim.fn.visualmode())
+    end, opts)
+  end,
 }
