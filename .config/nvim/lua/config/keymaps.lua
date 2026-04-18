d4dbacdc988240f47b2d62d82950d9c2a3846aa4
xuehui <xuehuizzz103@gmail.com>
2026-04-18 13:36:34 +0800

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,36 +1,38 @@
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
+
+local function map(mode, lhs, rhs, desc)
+  keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+end
 
 -- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>P", '"0P')
-keymap.set("v", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
+map("n", "x", '"_x', "Delete char without yanking")
+map("n", "<Leader>p", '"0p', "Paste from yank register (after)")
+map("n", "<Leader>P", '"0P', "Paste from yank register (before)")
+map("v", "<Leader>p", '"0p', "Paste from yank register (visual)")
+map("n", "<Leader>c", '"_c', "Change without yanking")
+map("n", "<Leader>C", '"_C', "Change to EOL without yanking")
+map("v", "<Leader>c", '"_c', "Change without yanking (visual)")
+map("v", "<Leader>C", '"_C', "Change to EOL without yanking (visual)")
+map("n", "<Leader>d", '"_d', "Delete without yanking")
+map("n", "<Leader>D", '"_D', "Delete to EOL without yanking")
+map("v", "<Leader>d", '"_d', "Delete without yanking (visual)")
+map("v", "<Leader>D", '"_D', "Delete to EOL without yanking (visual)")
 
--- 可视模式下的 Tab 缩进
-keymap.set("v", "<Tab>", ">gv", opts)
-keymap.set("v", "<S-Tab>", "<gv", opts)
+-- Tab indent (visual mode)
+map("v", "<Tab>", ">gv", "Indent and reselect")
+map("v", "<S-Tab>", "<gv", "Outdent and reselect")
 
--- 普通模式下的 Tab 缩进
-keymap.set("n", "<Tab>", ">>_", opts)
-keymap.set("n", "<S-Tab>", "<<_", opts)
+-- Tab indent (normal mode)  NOTE: overrides <C-i> (jumplist forward) in terminal
+map("n", "<Tab>", ">>_", "Indent current line")
+map("n", "<S-Tab>", "<<_", "Outdent current line")
 
--- 设置 Ctrl+A 全选
-keymap.set("n", "<C-a>", "ggVG", opts)
-keymap.set({ "i", "v" }, "<C-a>", "<Esc>ggVG", opts)
+-- Select all  NOTE: overrides default <C-a> (increment number)
+map("n", "<C-a>", "ggVG", "Select all")
+map({ "i", "v" }, "<C-a>", "<Esc>ggVG", "Select all")
 
--- 禁用 Command+A
-keymap.set({ "n", "i", "v" }, "<D-a>", "<Nop>", opts)
+-- Disable Command+A
+map({ "n", "i", "v" }, "<D-a>", "<Nop>", "Disable Cmd+A")
 
--- 复制当前行并粘贴到下一行
-keymap.set("n", "<C-d>", "yyp", opts)
-keymap.set("i", "<C-d>", "<Esc>yypi", opts)
-
+-- Duplicate line  NOTE: overrides default <C-d> (scroll half page down)
+map("n", "<C-d>", "yyp", "Duplicate line below")
+map("i", "<C-d>", "<Esc>yypa", "Duplicate line below (insert)")
