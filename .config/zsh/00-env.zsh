2275f74400db6cb3ccd77b8e24dd618dd7b86509
xuehui <xuehuizzz103@gmail.com>
2026-04-24 00:09:28 +0800

    Update 00-env.zsh

--- .config/zsh/00-env.zsh
+++ .config/zsh/00-env.zsh
@@ -2,6 +2,9 @@ export XDG_CONFIG_HOME="$HOME/.config"  # 配置文件
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
+# # 优先使用 ~/.local/bin 中的用户级工具（如 pip install --user 安装的命令）
+export PATH="$HOME/.local/bin:$PATH"
+
 # homebrew conf
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 export HOMEBREW_AUTO_UPDATE_SECS=1296000  # 15天自动更新一次
@@ -18,3 +21,4 @@ export LC_ALL="en_US.UTF-8"
 
 # less conf
 export LESS="-R -F -X"  # -R 处理颜色 -F 短输出时直接打印 -X 不清屏
+
