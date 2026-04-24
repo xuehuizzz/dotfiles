34b630f8559b5de67a6767fa6dd30d6048b1044b
xuehui <xuehuizzz103@gmail.com>
2026-04-24 10:23:19 +0800

    Update prompt.zsh

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,21 +1,44 @@
-# 基础
 setopt PROMPT_SUBST
-autoload -Uz vcs_info add-zsh-hook
 autoload -U colors && colors
+autoload -Uz add-zsh-hook
 
-# Git（极简 + 快）
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git:*' check-for-changes false
-zstyle ':vcs_info:git:*' formats ' %F{magenta} %b%f'
+# ---- git 异步（修正版）----
+typeset -g _git_branch=""
+typeset -g _git_async_fd=""
 
-add-zsh-hook precmd vcs_info
-
-# 智能路径（最多显示最后3段）
-_prompt_path() {
-  local p=${PWD/#$HOME/~}
-  echo ${(j:/:)${(s:/:)p}[-3,-1]}
+_git_async_start() {
+  _git_branch=""
+  [[ -n $_git_async_fd ]] && {
+    zle -F $_git_async_fd 2>/dev/null
+    exec {_git_async_fd}<&- 2>/dev/null
+  }
+  exec {_git_async_fd}< <(
+    git symbolic-ref --short HEAD 2>/dev/null \
+      || git rev-parse --short HEAD 2>/dev/null
+  )
+  zle -F $_git_async_fd _git_async_done
 }
 
-# PROMPT
-PROMPT='%F{cyan}$(_prompt_path)%f${vcs_info_msg_0_}
+_git_async_done() {
+  local fd=$1
+  _git_branch="$(<&$fd)"
+  zle -F $fd
+  exec {fd}<&-
+  _git_async_fd=""
+  zle reset-prompt
+}
+
+add-zsh-hook precmd _git_async_start
+
+# ---- 颜色 ----
+SEG_A_BG=238; SEG_B_BG=240
+SEG_A_FG=cyan; SEG_B_FG=205
+
+# ---- prompt ----
+_git_seg() {
+  [[ -z $_git_branch ]] && return
+  print -rn -- "%K{$SEG_B_BG}%F{$SEG_A_BG}%f%F{$SEG_B_FG}  $_git_branch%f "
+}
+
+PROMPT='%K{$SEG_A_BG} %F{$SEG_A_FG}%(4~|.../%3~|%~)%f $(_git_seg)%k
 %F{green}❯%f '
