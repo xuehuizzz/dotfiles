eceb02976bd7e56a186eab9d7c76c00a5a290981
xuehui <xuehuizzz103@gmail.com>
2026-04-24 10:06:02 +0800

    Update prompt.zsh

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,17 +1,21 @@
-# 基础设置
+# 基础
 setopt PROMPT_SUBST
-zmodload zsh/datetime          # 提供 $EPOCHREALTIME
 autoload -Uz vcs_info add-zsh-hook
 autoload -U colors && colors
 
-# Git 信息 (关闭 dirty check 以加速 prompt)
+# Git（极简 + 快）
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git:*' check-for-changes false
-zstyle ':vcs_info:git:*' formats ' %F{magenta} %b%f'
+zstyle ':vcs_info:git:*' formats ' %F{magenta} %b%f'
 
-# add-zsh-hook 自动去重, 比手动维护数组更健壮
 add-zsh-hook precmd vcs_info
 
+# 智能路径（最多显示最后3段）
+_prompt_path() {
+  local p=${PWD/#$HOME/~}
+  echo ${(j:/:)${(s:/:)p}[-3,-1]}
+}
+
 # PROMPT
-PROMPT='%F{cyan}%3~%f${vcs_info_msg_0_}${_cmd_duration}
+PROMPT='%F{cyan}$(_prompt_path)%f${vcs_info_msg_0_}
 %F{green}❯%f '
